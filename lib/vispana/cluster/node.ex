2e5e093fe0aed9861ec512475d459104e7f8d4eb
Broknloop <unknownbr+ghe@gmail.com>
2021-09-16 01:01:30 +0200

    Stop using Ecto in Cluster.Node

--- lib/vispana/cluster/node.ex
+++ lib/vispana/cluster/node.ex
@@ -1,12 +1,3 @@
 defmodule Vispana.Cluster.Node do
-  use Ecto.Schema
-
-  schema "nodes" do
-    field :content, :map
-    field :hostname, :string
-    field :serviceTypes, {:array, :string}
-    field :vespaId, :integer
-
-    timestamps()
-  end
+  defstruct [:id, :content, :hostname, :serviceTypes, :vespaId]
 end
