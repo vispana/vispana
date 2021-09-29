66b6505ece990aa7ba63cd9a3dd2e17584ca3d9d
Broknloop <unknownbr+ghe@gmail.com>
2021-09-29 02:24:07 +0200

    Small tweaks

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -30,17 +30,17 @@ module.exports = {
   },
   variants: {},
   plugins: [
-    // require('tailwindcss-animatecss')({
-    //   settings: {
-    //     animatedSpeed: 1000,
-    //     heartBeatSpeed: 1000,
-    //     hingeSpeed: 2000,
-    //     bounceInSpeed: 750,
-    //     bounceOutSpeed: 750,
-    //     animationDelaySpeed: 1000
-    //   },
-    //   variants: ['responsive'],
-    // }),
+    require('tailwindcss-animatecss')({
+      settings: {
+        animatedSpeed: 1000,
+        heartBeatSpeed: 1000,
+        hingeSpeed: 2000,
+        bounceInSpeed: 750,
+        bounceOutSpeed: 750,
+        animationDelaySpeed: 1000
+      },
+      variants: ['responsive'],
+    }),
   ],
   purge: [
     "../lib/**/*.eex",
