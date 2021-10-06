9a51f04803c9416504aba43520954ca6cef0c45a
Broknloop <unknownbr+ghe@gmail.com>
2021-10-06 14:09:46 +0200

    Add empty_cluster function

--- lib/vispana/cluster/vespacluster.ex
+++ lib/vispana/cluster/vespacluster.ex
@@ -1,3 +1,11 @@
 defmodule Vispana.Cluster.VespaCluster do
   defstruct [:configCluster, :containerCluster, :contentClusters]
+
+  alias Vispana.Cluster.VespaCluster
+  alias Vispana.Cluster.ConfigCluster
+  alias Vispana.Cluster.ContainerCluster
+
+  def empty_cluster do
+    %VespaCluster{configCluster: %ConfigCluster{}, containerCluster: %ContainerCluster{}, contentClusters: []}
+  end
 end
