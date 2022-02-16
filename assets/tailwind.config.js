02640ca9a5dd1783c02b2ddd3c096fe7885e42b9
Broknloop <unknownbr+ghe@gmail.com>
2022-02-16 02:28:49 +0100

    Fix tailwind when building from scratch

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -35,18 +35,7 @@ module.exports = {
   },
   variants: {},
   plugins: [
-    require('daisyui'),
-    require('tailwindcss-animatecss')({
-      settings: {
-        animatedSpeed: 1000,
-        heartBeatSpeed: 1000,
-        hingeSpeed: 2000,
-        bounceInSpeed: 750,
-        bounceOutSpeed: 750,
-        animationDelaySpeed: 1000
-      },
-      variants: ['responsive']
-    })
+    require('daisyui')
   ],
   purge: { 
     options: {
