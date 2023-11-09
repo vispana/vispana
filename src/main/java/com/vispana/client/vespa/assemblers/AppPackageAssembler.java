efb63221a9ed044c87981b95845d30b567c8ae0c
broknloop <90255202+broknloop@users.noreply.github.com>
2023-11-09 18:01:25 +0100

    fix(hosts-xml-missing): Fix issue when Vispana fails to load hosts.xml, when not available (#48)
    Co-authored-by: Broknloop <broknloop@gmail.com>

--- src/main/java/com/vispana/client/vespa/assemblers/AppPackageAssembler.java
+++ src/main/java/com/vispana/client/vespa/assemblers/AppPackageAssembler.java
@@ -1,6 +1,7 @@
 package com.vispana.client.vespa.assemblers;
 
 import static com.vispana.client.vespa.helpers.Request.request;
+import static com.vispana.client.vespa.helpers.Request.requestWithDefaultValue;
 
 import com.vispana.api.model.apppackage.ApplicationPackage;
 import com.vispana.client.vespa.model.ApplicationSchema;
@@ -9,10 +10,10 @@ public class AppPackageAssembler {
 
   public static ApplicationPackage assemble(String appUrl) {
     var appSchema = request(appUrl, ApplicationSchema.class);
-    var hostContent = request(appUrl + "/content/hosts.xml", String.class);
+    var hostContent = requestWithDefaultValue(appUrl + "/content/hosts.xml", String.class, "");
     var servicesContent = request(appUrl + "/content/services.xml", String.class);
 
     return new ApplicationPackage(
-        appSchema.getGeneration().toString(), servicesContent, hostContent);
+        appSchema.getGeneration().toString(), servicesContent, hostContent.orElse(""));
   }
 }
--- src/main/java/com/vispana/client/vespa/helpers/Request.java
+++ src/main/java/com/vispana/client/vespa/helpers/Request.java
@@ -1,11 +1,22 @@
 package com.vispana.client.vespa.helpers;
 
+import java.util.Optional;
 import org.springframework.web.client.RestClient;
 
 public class Request {
+
   private static final RestClient restClient = RestClient.create();
 
   public static <T> T request(String url, Class<T> responseType) {
     return restClient.get().uri(url).retrieve().body(responseType);
+  }
+
+  public static <T> Optional<T> requestWithDefaultValue(
+      String url, Class<T> responseType, T defaultValue) {
+    try {
+      return Optional.of(request(url, responseType));
+    } catch (Exception exception) {
+      return Optional.of(defaultValue);
+    }
   }
 }
